b126ea22d53d54c75120dbd35bdbe5af09f17e8f
theankomah <128159662+theankomah@users.noreply.github.com>
2023-08-10 18:53:45 +0000

    Updated sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://23.22.244.182:8000/',
+  serverUrl: 'http://172.31.33.235:7000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '73e279fcb46164fb832e53de6d23af5bc2f783f9',
-	    //'sonar.password': 'admin',
+	    'sonar.login': 'ae5c5ab1ac3d74c470b9ff18c3f32cce9c07fc88',
+	    //'sonar.password': 'jireh@Ankomah1',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
